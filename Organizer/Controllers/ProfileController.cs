2f888a98a26d3343d80dbc573f0adbc9b74d666a
HamidOmarr <you@example.com>
2024-03-22 11:33:52 +0100

    -Implemented functionality for the sign-out button. -Modified profile to fetch and display user-specific information.

--- Organizer/Controllers/ProfileController.cs
+++ Organizer/Controllers/ProfileController.cs
@@ -1,23 +1,26 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using Microsoft.Graph;
-using Organizer.Models;
-using Organizer.Services;
-using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Organizer.Controllers
 {
-   
+    [Authorize]
     public class ProfileController : Controller
     {
         public ActionResult Index()
-        { 
-
+        {
             // Pass the user profile data to the view
             return View();
         }
 
-        
+        // Sign out action
+        public IActionResult SignOut()
+        {
+            // Perform sign-out
+            HttpContext.SignOutAsync();
+
+            // Redirect to home page or any other page after sign-out
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
