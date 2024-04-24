a8517076ca803ee48c9e7f9c3a4888f7615e8de2
Amar Nagim <amarnagim@Hotmail.com>
2024-04-24 10:58:20 +0200

    removed a duplicate action in usercontroller

--- Organizer/Controllers/UsersController.cs
+++ Organizer/Controllers/UsersController.cs
@@ -130,12 +130,6 @@ namespace Organizer.Views.Shared.Controllers
             return View();
         }
 
-        public IActionResult CompanyAdminDashboard()
-        {/*
-     var viewModel = new PageIdentifier();
-     viewModel.PageValue = "Profile";*/
-            return View();
-        }
 
         public async Task<IActionResult> Teams()
         {
