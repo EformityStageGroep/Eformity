97ccc4cc9d384fd1904a19df18fb438a6e39d460
StefanNijssen <99053355@mydavinci.nl>
2024-05-15 13:29:22 +0200

    clean up

--- Organizer/Controllers/HomeController.cs
+++ Organizer/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using Microsoft.Graph;
-using Organizer.Contexts;
 using Organizer.Models;
 using Organizer.Repositories;
-using Organizer.Services;
 using System.Diagnostics;
 
 namespace Organizer.Controllers
@@ -14,58 +11,21 @@ namespace Organizer.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        SqlCommand com = new SqlCommand();
-        SqlDataReader dr;
+
         SqlConnection con = new SqlConnection();
        // List<Databron> Databronnen = new List<Databron>();
-        private readonly ILogger<HomeController> _logger;
-        private readonly IGraphClientService _graphClientService;
-        private readonly GraphServiceClient graphServiceClient;
-        private readonly ITeamsRepository _teamRepository;
-        private readonly IUserRepository _userRepository;
-        private readonly IEmployeeRepository _taskRepository;
         private readonly IEmployeeRepository _employeeRepository;
-        private readonly OrganizerContext _context;
 
-        public HomeController(ITeamsRepository teamRepository, IUserRepository userRepository, IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository)
         {
-            _teamRepository = teamRepository;
-            _userRepository = userRepository;
-            _taskRepository = employeeRepository;
             _employeeRepository = employeeRepository;
-        }
-        public HomeController(ILogger<HomeController> logger, IGraphClientService graphClientService)
-        {
-            _logger = logger;
-            _graphClientService = graphClientService;
-            graphServiceClient = _graphClientService.GetGraphServiceClient();
             con.ConnectionString = Properties.Resources.ConnectionString;
         }
-
+      
         [Authorize(Roles = "SuperAdmin,Employee")]
         public IActionResult Index()
         {
-            if (User.IsInRole("SuperAdmin"))
-            {
-                return RedirectToAction("EmployeeDashboard", "Employee");
-            }
-            else if (User.IsInRole("CompanyAdmin"))
-            {
-                return View("CompanyAdminDashboard");
-            }
-            else if (User.IsInRole("EmployeeAdmin"))
-            {
-                return View("EmployeeAdminDashboard");
-            }
-            else if (User.IsInRole("Employee"))
-            {
-                return View("EmployeeDashboard");
-            }
-            else
-            {
-                // Handle other roles or unauthorized access
-                return RedirectToAction("Unauthorized", "Error");
-            }
+            return View();
         }
 
         public async Task<IActionResult> Homepage()
