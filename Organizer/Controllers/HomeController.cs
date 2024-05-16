132fff1b1410a92735b6292cfe9d2ef701ca3177
Amar Nagim <amarnagim@Hotmail.com>
2024-05-16 15:53:52 +0200

    Added changes. Still need to fix a few bugs

--- Organizer/Controllers/HomeController.cs
+++ Organizer/Controllers/HomeController.cs
@@ -1,101 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using Microsoft.Graph;
-using Organizer.Contexts;
 using Organizer.Models;
 using Organizer.Repositories;
-using Organizer.Services;
 using System.Diagnostics;
 
 namespace Organizer.Controllers
 {
- 
+
     [Authorize]
     public class HomeController : Controller
     {
-        SqlCommand com = new SqlCommand();
-        SqlDataReader dr;
-        SqlConnection con = new SqlConnection();
-       // List<Databron> Databronnen = new List<Databron>();
-        private readonly ILogger<HomeController> _logger;
-        private readonly IGraphClientService _graphClientService;
-        private readonly GraphServiceClient graphServiceClient;
-        private readonly ITeamsRepository _teamRepository;
-        private readonly IUserRepository _userRepository;
-        private readonly ITasksRepository _taskRepository;
-        private readonly OrganizerContext _context;
 
-        public HomeController(ITeamsRepository teamRepository, IUserRepository userRepository, ITasksRepository tasksRepository)
+        SqlConnection con = new SqlConnection();
+        // List<Databron> Databronnen = new List<Databron>();
+        private readonly ITasksRepository _tasksRepository;
+
+        public HomeController(ITasksRepository tasksRepository)
         {
-            _teamRepository = teamRepository;
-            _userRepository = userRepository;
-            _taskRepository = tasksRepository;
-        }
-        public HomeController(ILogger<HomeController> logger, IGraphClientService graphClientService)
-        {
-            _logger = logger;
-            _graphClientService = graphClientService;
-            graphServiceClient = _graphClientService.GetGraphServiceClient();
-            con.ConnectionString = Organizer.Properties.Resources.ConnectionString;
+            _tasksRepository = tasksRepository;
+            con.ConnectionString = Properties.Resources.ConnectionString;
         }
 
         [Authorize(Roles = "SuperAdmin,Tasks")]
         public IActionResult Index()
         {
-            if (User.IsInRole("SuperAdmin"))
-            {
-                return RedirectToAction("TasksDashboard", "Tasks");
-            }
-            else if (User.IsInRole("CompanyAdmin"))
-            {
-                return View("CompanyAdminDashboard");
-            }
-            else if (User.IsInRole("TasksAdmin"))
-            {
-                return View("TasksAdminDashboard");
-            }
-            else if (User.IsInRole("Tasks"))
-            {
-                return View("TasksDashboard");
-            }
-            else
-            {
-                // Handle other roles or unauthorized access
-                return RedirectToAction("Unauthorized", "Error");
-            }
-
+            return View();
         }
-
-
 
         public async Task<IActionResult> Homepage()
         {
+            var ParentViewModel = await _tasksRepository.ParentViewModel("Homepage");
 
-
-            ParentViewModel mymodel = new ParentViewModel();
-            List<Entities.User> users = await _userRepository.GetUserIdsByTenant();
-            List<Entities.Team> teams = await _teamRepository.GetTeamsByUser();
-            List<Entities.Task> tasks = await _taskRepository.GetTasksAsync();
-
-            var model = new ParentViewModel
-            {
-                Users = users,
-                Teams = teams,
-                Tasks = tasks
-            };
-            return View(model);
+            // Return the view with the model
+            return View(ParentViewModel);
         }
 
-        public IActionResult Profile()
-        {
-/*            var viewModel = new PageIdentifier();
-            viewModel.PageValue = "Profile";*/
-            return View();
-        }
-        
-      
-   
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
