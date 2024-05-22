76597c156ad1fab4320f6633a9679e1e363d8a15
StefanNijssen <99053355@mydavinci.nl>
2024-05-22 14:10:03 +0200

    settings fixed

--- Organizer/Models/Databron.cs
+++ Organizer/Models/Databron.cs
@@ -5,10 +5,12 @@ namespace Organizer.Models
     public class ParentViewModel
     {
         public List<Entities.User> Users { get; set; }
+        public List<Entities.User> CurrentUser { get; set; }
         public List<Entities.Team> Teams { get; set; }
         public List<Entities.Task> Tasks { get; set; }
         public List<Entities.Role> Roles { get; set; }
         public Entities.PageIdentifier PageIdentifier { get; set; }
+        
     }
     public class TeamTaskModel
     {
